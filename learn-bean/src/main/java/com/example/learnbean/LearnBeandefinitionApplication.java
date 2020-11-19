c9ba6435f1f3a15b1062e6e2c4de1a9432d36451
Hardy <weiha2@oocl.com>
2020-11-19 17:23:00 +0800

    [wtzpt] feature : init project

---
+++ learn-bean/src/main/java/com/example/learnbean/LearnBeandefinitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBeandefinitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnBeandefinitionApplication.class, args);
+	}
+
+}
---
+++ learn-bean/src/test/java/com/example/learnbean/LearnBeandefinitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBeandefinitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
