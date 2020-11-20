47f455ee62193ba4ec54107ca4ed63f5c470160b
Hardy <weiha2@oocl.com>
2020-11-20 16:36:19 +0800

    [wtzpt] refactor:  rebuild learn-ioc module

---
+++ learn-ioc/src/main/java/com/example/learnioc/LearnIocApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnIocApplication.class, args);
+	}
+
+}
